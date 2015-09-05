1faa6b14dad1f0d7ce6f1dcc197e420c16623eae
Niklas Mähler <niklas.mahler@gmail.com>
2015-09-05 22:00:32 +0200

    Add install file
    Currently it only installs the git aliases in a quite naive manner,
    but in the future it will install all other dotfiles in a system
    specific manner.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+set -eu
+
+# Get script directory: http://stackoverflow.com/a/246128/885443
+DOTFILE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+if [ ! -e "$HOME/.gitconfig" ] || \
+        ! grep -q "path \= .*gitconfig\.alias$" "$HOME/.gitconfig"; then
+    echo "[include]" >> "$HOME/.gitconfig"
+    printf "\t%s\n" "path = ${DOTFILE_PATH}/gitconfig.alias" >> "${HOME}/.gitconfig"
+fi
+
