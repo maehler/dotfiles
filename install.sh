e6ae0f37e72aef60dcc0230b71ebdcd60fa2c646
Niklas Mähler <niklas.mahler@gmail.com>
2015-09-06 19:34:54 +0200

    Add comments and shorten lines

--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@ set -eu
 # Get script directory: http://stackoverflow.com/a/246128/885443
 DOTFILE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+# Add git aliases to ~/.gitconfig
 if [ ! -e "$HOME/.gitconfig" ] || \
         ! grep -q "path \= .*gitconfig\.alias$" "$HOME/.gitconfig"; then
     echo "[include]" >> "$HOME/.gitconfig"
-    printf "\t%s\n" "path = ${DOTFILE_PATH}/gitconfig.alias" >> "${HOME}/.gitconfig"
+    printf "\t%s\n" "path = ${DOTFILE_PATH}/gitconfig.alias" >> \
+        "${HOME}/.gitconfig"
 fi
 
