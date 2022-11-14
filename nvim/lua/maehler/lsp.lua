472781c709142701d60d2c34e410fbf9a876659c
Niklas Mähler <niklas.mahler@regionvasterbotten.se>
2022-11-14 08:51:12 +0100

    Add ts/js LSP

--- nvim/lua/maehler/lsp.lua
+++ nvim/lua/maehler/lsp.lua
@@ -38,3 +38,8 @@ require("lspconfig").rust_analyzer.setup({
     on_attach = on_attach,
     flags = lsp_flags,
 })
+
+require("lspconfig").tsserver.setup({
+    on_attach = on_attach,
+    flags = lsp_flags,
+})
