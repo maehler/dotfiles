aa147ab2ff51a4002171353b6b0f9a707be3ab09
Niklas Mähler <niklas.mahler@regionvasterbotten.se>
2023-01-02 22:24:35 +0100

    Add gopls

--- nvim/lua/maehler/lsp.lua
+++ nvim/lua/maehler/lsp.lua
@@ -43,3 +43,8 @@ require("lspconfig").tsserver.setup({
     on_attach = on_attach,
     flags = lsp_flags,
 })
+
+require("lspconfig").gopls.setup({
+    on_attach = on_attach,
+    flags = lsp_flags,
+})
