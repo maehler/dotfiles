c1da1e418ea71991da81bdcfc9ef4cffa04159f6
Niklas Mähler <niklas.mahler@regionvasterbotten.se>
2023-02-22 10:41:18 +0100

    Recenter after page up/down

--- nvim/lua/maehler/remap.lua
+++ nvim/lua/maehler/remap.lua
@@ -16,3 +16,7 @@ vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>", {})
 
 -- Paste and keep the item in the default register
 vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Center after going up/down
+vim.keymap.set("n", "<C-u>", "<C-u>zz");
+vim.keymap.set("n", "<C-d>", "<C-d>zz");
