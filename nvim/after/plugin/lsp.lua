8f3333ef1fb1865ad2072342833b3d517b77f158
Niklas Mähler <niklas.mahler@gmail.com>
2023-03-21 10:30:23 +0100

    Better lsp autocompletion triggers

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,8 +1,28 @@
-local lsp = require("lsp-zero").preset({
+local lsp = require("lsp-zero")
+
+lsp.preset({
     name = "minimal",
     set_lsp_keymaps = true,
     manage_nvim_cmp = true,
-    suggest_lsp_servers = false,
+    suggest_lst_servers = false,
+})
+
+local cmp = require("cmp")
+
+local cmp_select = { behaviour = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ["<Tab>"] = vim.NIL,
+    ["<S-Tab>"] = vim.NIL,
+    ["<CR>"] = vim.NIL,
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
 })
 
 lsp.setup()
