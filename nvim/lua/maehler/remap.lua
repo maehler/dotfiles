8b177973f2eb18b774ea10c5579df84f0fed6b55
Niklas Mähler <niklas.mahler@gmail.com>
2023-03-21 10:29:39 +0100

    Harpoon remaps

--- nvim/lua/maehler/remap.lua
+++ nvim/lua/maehler/remap.lua
@@ -5,12 +5,6 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 
--- Harpoon
-vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file, {})
-vim.keymap.set("n", "<leader>hq", require("harpoon.ui").toggle_quick_menu, {})
-vim.keymap.set("n", "<leader>hn", require("harpoon.ui").nav_next, {})
-vim.keymap.set("n", "<leader>hp", require("harpoon.ui").nav_prev, {})
-
 -- Netrw
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>", {})
 
