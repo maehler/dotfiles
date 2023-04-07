c393aa8818961d2dff78e61feec959a52ba33827
Niklas Mähler <niklas.mahler@gmail.com>
2023-04-07 23:25:26 +0200

    Add indent guides

--- nvim/lua/maehler/packer.lua
+++ nvim/lua/maehler/packer.lua
@@ -44,6 +44,9 @@ return require("packer").startup(function()
         "Mxrcon/nextflow-vim",
     }
 
+    -- Indentation guides
+    use "lukas-reineke/indent-blankline.nvim"
+
     -- LSP
     use {
         'VonHeikemen/lsp-zero.nvim',
