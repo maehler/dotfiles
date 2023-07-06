06bc467bebf9f23c7557522d13b7ef4df1f5ef3c
Niklas Mähler <niklas.mahler@gmail.com>
2023-07-06 23:24:08 +0200

    Fix heirline git

--- nvim/after/plugin/heirline.lua
+++ nvim/after/plugin/heirline.lua
@@ -89,21 +89,21 @@ local Git = {
             local count = self.status_dict.added or 0
             return count > 0 and ("+" .. count)
         end,
-        hl = { fg = "git_add" },
+        hl = { fg = "green" },
     },
     {
         provider = function(self)
             local count = self.status_dict.removed or 0
             return count > 0 and ("-" .. count)
         end,
-        hl = { fg = "git_del" },
+        hl = { fg = "red" },
     },
     {
         provider = function(self)
             local count = self.status_dict.changed or 0
             return count > 0 and ("~" .. count)
         end,
-        hl = { fg = "git_change" },
+        hl = { fg = "yellow" },
     },
     {
         condition = function(self)
