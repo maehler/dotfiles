69f2fb24814aefa540fcfd4a9777604176649249
Niklas Mähler <niklas.mahler@gmail.com>
2025-04-07 22:00:42 +0200

    Fix treesitter incremental selection

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -7,10 +7,8 @@ require("nvim-treesitter.configs").setup({
 	incremental_selection = {
 		enable = true,
 		keymaps = {
-			init_selection = "<c-space>",
-			node_incremental = "<c-space>",
-			scope_incremental = "<c-s>",
-			node_decremental = "<M-space>",
+			node_incremental = "v",
+			node_decremental = "V",
 		},
 	},
 	textobjects = {
