e03c7523d790a483ad245511e5de2a61438e4123
Niklas Mähler <niklas.mahler@gmail.com>
2025-09-17 11:20:29 +0200

    Show relative path to file in lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -6,4 +6,12 @@ require("lualine").setup({
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 	},
+	sections = {
+		lualine_c = {
+			{
+				"filename",
+				path = 1,
+			}
+		},
+	},
 })
